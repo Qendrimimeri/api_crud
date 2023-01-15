793e60de9dab65ded2a044184b42e8135bc32113
Qendrim Imeri <qendrim.s.imeri@gmail.com>
2023-01-15 12:07:36 +0100

    Add project files.

---
+++ ToDo/Controllers/ToDoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDoController : ControllerBase
+{
+    [HttpPost("addtodo")]
+    public IActionResult AddToDo(ToDoModel model)
+    {
+        return Ok(model);
+    }
+}
---
+++ ToDo/Models/ToDoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDo.Models;
+
+public class ToDoModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
